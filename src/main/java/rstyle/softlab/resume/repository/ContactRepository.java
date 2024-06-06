415a9f3728302dd6285dac57fcef389e3fe611e0
Ilyas <xxxilyas92xxx@gmail.com>
2024-06-06 10:56:38 +0500

    R-Style Softlab Исправления подключения к БД

--- src/main/java/rstyle/softlab/resume/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package rstyle.softlab.resume.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rstyle.softlab.resume.model.entity.contact.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
--- src/main/resources/db/changelog/changelog-1.0.sql
+++ src/main/resources/db/changelog/changelog-1.0.sql
@@ -4,22 +4,36 @@
 --
 CREATE TABLE IF NOT EXISTS resume
 (
-    id         BIGSERIAL    NOT NULL PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    contact_id BIGINT       NOT NULL,
-    CONSTRAINT fk_resume_contact FOREIGN KEY (contact_id) REFERENCES contacts (id)
+    id   BIGSERIAL    NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
-
 --liquibase formatted sql
 --
 -- Create Projects table
 --
 CREATE TABLE IF NOT EXISTS projects
 (
-    id          BIGSERIAL    NOT NULL PRIMARY KEY,
+    id          BIGSERIAL    NOT NULL,
     name        VARCHAR(255) NOT NULL,
     description TEXT,
-    link        VARCHAR(255)
+    link        VARCHAR(255),
+    resume_id   BIGINT       NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_project_resume FOREIGN KEY (resume_id) REFERENCES resume (id)
+);
+--liquibase formatted sql
+--
+-- Create Company table
+--
+CREATE TABLE IF NOT EXISTS company
+(
+    id          BIGSERIAL    NOT NULL,
+    name        VARCHAR(255) NOT NULL,
+    position    VARCHAR(255),
+    department  VARCHAR(255),
+    description TEXT,
+    PRIMARY KEY (id)
 );
 --liquibase formatted sql
 --
@@ -27,100 +41,53 @@ CREATE TABLE IF NOT EXISTS projects
 --
 CREATE TABLE IF NOT EXISTS experience
 (
-    id         BIGSERIAL NOT NULL PRIMARY KEY,
+    id         BIGSERIAL NOT NULL,
     company_id BIGINT    NOT NULL,
     start_date DATE      NOT NULL,
     end_date   DATE,
     experience INT,
-
-    CONSTRAINT fk_experience_company FOREIGN KEY (company_id) REFERENCES company (id)
+    resume_id  BIGINT    NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_experience_company FOREIGN KEY (company_id) REFERENCES company (id),
+    CONSTRAINT fk_experience_resume FOREIGN KEY (resume_id) REFERENCES resume (id)
 );
 
---liquibase formatted sql
---
--- Create Company table
---
-CREATE TABLE IF NOT EXISTS company
-(
-    id          BIGSERIAL    NOT NULL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description TEXT
-);
---liquibase formatted sql
---
--- Create Institution table
---
 --liquibase formatted sql
 --
 -- Create Institution table
 --
 CREATE TABLE IF NOT EXISTS institution
 (
-    id      BIGSERIAL    NOT NULL PRIMARY KEY,
+    id      BIGSERIAL    NOT NULL,
     name    VARCHAR(255) NOT NULL,
-    address  VARCHAR (255),
+    address VARCHAR(255),
     phone   VARCHAR(255),
     email   VARCHAR(255),
-    website VARCHAR(255)
+    website VARCHAR(255),
+    PRIMARY KEY (id)
 );
-
 --liquibase formatted sql
 --
 -- Create Education table
 --
 CREATE TABLE IF NOT EXISTS education
 (
-    id             BIGSERIAL NOT NULL PRIMARY KEY,
+    id             BIGSERIAL NOT NULL,
     is_graduated   BOOLEAN   NOT NULL,
     start_date     DATE      NOT NULL,
     end_date       DATE,
     institution_id BIGINT    NOT NULL,
-        CONSTRAINT fk_education_institution FOREIGN KEY (institution_id) REFERENCES institution (id)
-);
---liquibase formatted sql
---
--- Create Contacts table
---
-CREATE TABLE contacts
-(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    PRIMARY KEY (id)
-);
---liquibase formatted sql
---
--- Create contact_phones table
---
-CREATE TABLE contact_phones
-(
-    contact_id   BIGINT       NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
-    PRIMARY KEY (contact_id, phone_number),
-    CONSTRAINT fk_contact_phones_contact FOREIGN KEY (contact_id) REFERENCES contacts (id)
+    resume_id      BIGINT    NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_education_institution FOREIGN KEY (institution_id) REFERENCES institution (id),
+    CONSTRAINT fk_education_resume FOREIGN KEY (resume_id) REFERENCES resume (id)
 );
 
---liquibase formatted sql
---
--- Create contact_emails table
---
-CREATE TABLE contact_emails
-(
-    contact_id    BIGINT       NOT NULL,
-    email_address VARCHAR(255) NOT NULL,
-    PRIMARY KEY (contact_id, email_address),
-    CONSTRAINT fk_contact_emails_contact FOREIGN KEY (contact_id) REFERENCES contacts (id)
-);
 
---liquibase formatted sql
---
--- Create contact_links table
---
-CREATE TABLE contact_links
-(
-    contact_id BIGINT       NOT NULL,
-    link       VARCHAR(255) NOT NULL,
-    PRIMARY KEY (contact_id, link),
-    CONSTRAINT fk_contact_links_contact FOREIGN KEY (contact_id) REFERENCES contacts (id)
-);
+
+
+
+
 
 
 
